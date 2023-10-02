2d9b7a41d8d3ec4235a48266108eea0dcb834b36
B-Litljon <brandonC.littlejohn@gmail.com>
2023-10-01 18:11:05 -0700

    crud for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,10 +20,22 @@ const getUsers = asyncHandler(async (req, res) => {
 // @access private
 
 const createUser = asyncHandler(async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, email } = req.body;
+
+    // confirm data
+    if (!username || !password || !email) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+    // check if user already exists
+    const duplicate = await User.findOne({ username }).lean().exec();
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'User already exists' });
+    }
+    // hash password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const newUser = await user.create({ username, password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(password, salt); //salt the hash 10 times
+    const newUser = await user.create({ username, password: hashedPassword, email });
     res.json(newUser);
 });
 
