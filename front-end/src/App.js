56d83d33d68a1cba0786822fc7a3a60cc45738b8
B-Litljon <brandonC.littlejohn@gmail.com>
2023-11-11 03:12:56 -0800

    updated navbar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from 'navbar';
+import Navbar from './components/navbar';
+import 'bulma/css/bulma.css';
 
 function App() {
   return (
@@ -9,5 +10,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
