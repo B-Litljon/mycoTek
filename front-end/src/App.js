63ca50c41dcc7ac44a239051ea60625793953f45
B-litljon <brandonC.littlejohn@gmail.com>
2023-12-14 05:05:33 -0800

    Add Bulma CSS, signup component is made

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+import SignUpPage from './components/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="section">
+        <div className="container">
+          <SignUpPage />
+        </div>
+      </section>
     </div>
   );
 }
