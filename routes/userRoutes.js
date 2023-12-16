d25912800ea6698fb4d841b03519f9c0899bb907
B-litljon <brandonC.littlejohn@gmail.com>
2023-12-16 06:20:20 -0800

    Remove unnecessary whitespace in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,6 @@ const userController = require('../controllers/userController');
 router.route('/')
     .get(userController.getUsers)
     .post(userController.createUser)
-    
     .patch(userController.updateUser)
     .delete(userController.removeUser)
 
