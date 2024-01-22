6eb3647c5fbab129efd0959b45edde75ec1d82e7
B-litljon <brandonC.littlejohn@gmail.com>
2024-01-22 06:11:10 -0800

    Add expand/collapse functionality to TreeList component

--- front-end/src/components/TreeList.js
+++ front-end/src/components/TreeList.js
@@ -1,17 +1,28 @@
 
 import React, { useState } from "react";
+import TreeNode from "./TreeNode";
 
 const TreeList = ({ nodes }) => {
-    const [expanded, setExpanded] = useState(false);
-    const handleNodeClick = () => {
-        setExpanded(!expanded);
+    const [expandedNodes, setExpandedNodes] = useState({});
+
+    const handleNodeClick = (nodeIndex) => {
+        
+        const isExpanded = expandedNodes[nodeIndex];  
+        setExpandedNodes ({
+            ...expandedNodes,
+            [nodeIndex]: !isExpanded  // if node is expanded, collapse it; if node is collapsed, expand it
+        });
     };
     
     return (
         <>
             {nodes.map((node, index) => (
-                <TreeNode key = {index} label={node.label} onClick={handleNodeClick}>
-                    {expanded && <TreeList nodes={node.children} />}
+                <TreeNode 
+                    key = {index} 
+                    label={node.label} 
+                    onClick={() => handleNodeClick(index)}
+                >
+                    {expandedNodes[index] && node.children && <TreeList nodes={node.children} />}
                 </TreeNode>
             ))}
         </>
