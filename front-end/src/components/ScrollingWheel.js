5d18547e20b02261337684029917f8df502c5dc1
B-litljon <brandonC.littlejohn@gmail.com>
2024-01-22 05:49:31 -0800

    Fix rendering issue in ScrollingWheel component

--- front-end/src/components/ScrollingWheel.js
+++ front-end/src/components/ScrollingWheel.js
@@ -3,7 +3,7 @@ const ScrollingWheel = ({ items }) => {
     return (
         <div style = {{ overflowY: 'scroll', height: '300px'}}>
             {items.map((item, index) => (
-                <TreeList key={index} nodes={items}/>
+                <TreeList key={index} nodes={item}/>
             ))}
         </div>
     );
